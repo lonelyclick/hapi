13e2a26a7607daba3061135f48830ec1506bc725
Guang Yang <guang.yang@yohomobile.com>
2026-01-03 03:44:17 +0000

    deploy

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -56,7 +56,7 @@ export default defineConfig({
         react(),
         VitePWA({
             registerType: 'autoUpdate',
-            includeAssets: ['favicon.ico', 'apple-touch-icon-180x180.png', 'mask-icon.svg'],
+            includeAssets: ['favicon.ico', 'apple-touch-icon-180x180.png', 'pwa-192x192.png'],
             manifest: {
                 name: 'Yoho Remote',
                 short_name: 'Yoho',
