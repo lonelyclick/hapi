4bb65a39d54855f536335e936b3adecb9acb53d9
Guang Yang <guang.yang@yohomobile.com>
2026-01-05 09:16:20 +0000

    deploy

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -151,6 +151,7 @@ export default defineConfig({
                 skipWaiting: true,
                 clientsClaim: true,
                 globPatterns: ['**/*.{js,css,html,ico,png,svg,woff,woff2}'],
+                maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
                 // Import custom push notification handlers
                 importScripts: ['/sw-push.js'],
                 runtimeCaching: [
