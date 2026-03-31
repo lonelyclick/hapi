4e3681165c68e213a15330d131b94782c9f0a06e
Guang Yang <guang.yang@yohomobile.com>
2026-03-31 03:49:11 +0000

    deploy

--- web/src/routes/settings.tsx
+++ web/src/routes/settings.tsx
@@ -661,10 +661,8 @@ export default function SettingsPage() {
                                                 </div>
                                             </div>
                                         </div>
-                                        {isCurrent ? (
+                                        {isCurrent && (
                                             <CheckIcon className="shrink-0 text-[var(--app-button)]" />
-                                        ) : (
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="shrink-0 text-[var(--app-hint)]"><polyline points="9 18 15 12 9 6" /></svg>
                                         )}
                                     </button>
                                 )
